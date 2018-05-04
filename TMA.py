45584176f33cf6e1bb15e03a3bfc668f16b12a68
Avimanyu Bandyopadhyay <avimanyu786@users.noreply.github.com>
2018-05-04 21:10:26 +0530

    Update TMA.py

--- TMA.py
+++ TMA.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Tkinter,tkFileDialog,subprocess,os,tempfile
+import Tkinter,tkFileDialog,os
 
 root = Tkinter.Tk()
 root.withdraw()
