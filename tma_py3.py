9f756bb60f386eff95a1e00b8a2c1582c40c48f1
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2021-01-30 15:26:45 +0530

    Update tma_py3.py

--- tma_py3.py
+++ tma_py3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# TrajectoryMergeAssist v2.1
+# TrajectoryMergeAssist v2.1.1
 # A python3 based GUI tool that helps in merging two trajectories on Desmond MD
 
-# Copyright (C) 2019 Avimanyu Bandyopadhyay
+# Copyright (C) 2021 Avimanyu Bandyopadhyay
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
